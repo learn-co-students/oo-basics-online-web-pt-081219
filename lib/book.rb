a66e9c9a203feb2ea6409471c25724631df1cbd4
Sydnee Sampson <sbsydnee161416@gmail.com>
2019-08-28 18:43:34 +0000

    Done.

--- lib/book.rb
+++ lib/book.rb
@@ -1,2 +1,31 @@
-
-
+class Book 
+  attr_accessor :title, :author, :page_count, :genre
+def initialize(title)
+  @title = title 
+  end
+def turn_page
+  puts "Flipping the page...wow, you read fast!"
+end
+end
+#   def title 
+#   @title = "And Then There Were None"
+#   end
+#   def author 
+#     @author = "Agatha Christie"
+#   end
+# end 
+# class Book 
+#   def initialize(title,author,page_count,genre)
+#     @title = title 
+#     @author = author 
+#     @page_count = page_count
+#     @genre = genre
+# end
+# end
+#   def title 
+#   @title = "And Then There Were None"
+#   end
+#   def author 
+#     @author = "Agatha Christie"
+#   end
+# end 
